5491a2c13936c693930d56c11dbddf7dfd768a94
SingaraveluSStackRad <SingaraveluSStackRad@STACKRADIUSLT01>
2023-04-02 19:12:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// will VS Studio create the solution file for a Console app created in the CMD Line.
+
+
